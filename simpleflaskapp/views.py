08432672dbba38dde4ad00bd8e80a299896682e6
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-07 13:32:00 +1000

    install awscli and configure with env vars

--- simpleflaskapp/views.py
+++ simpleflaskapp/views.py
@@ -1,6 +1,7 @@
 import datetime
 from simpleflaskapp import app
-from flask import jsonify
+from flask import jsonify, render_template, make_response
+import subprocess
 
 @app.route('/', methods = ['GET'])
 def hello_world():
@@ -19,3 +20,11 @@ def get_date():
 def get_time():
     return jsonify({'time': str(datetime.datetime.now().time())})
 
+
+@app.route('/aws', methods = ['GET'])
+def get_aws_bucket():
+    cmd = "aws s3 ls s3://opencv-buildpack/demos/cnn-example/"
+    stdout = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
+    resp = make_response(stdout)
+    resp.headers['Content-type'] = 'text/plain; charset=utf-8'
+    return resp
