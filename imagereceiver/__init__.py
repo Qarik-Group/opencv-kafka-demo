7ed4a2d6a2bbfb1a35526a9192df05696787d4f7
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-10 16:40:24 +1000

    fall back to local kafka

--- imagereceiver/__init__.py
+++ imagereceiver/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask, jsonify, render_template, make_response
 from cfenv import AppEnv
 
 env = AppEnv()
-kafka = env.get_service(name='raw-images-topic')
+kafkaService = env.get_service(name='raw-images-topic')
+if kafkaService is None:
+    kafkaServers   = "localhost:9092"
+    kafkaTopicName = "raw-images-topic-demo"
 
-kafkaServers   = kafka.credentials.get("hostname")
-kafkaTopicName = kafka.credentials.get("topicName")
+else:
+    kafkaServers   = kafkaService.credentials.get("hostname")
+    kafkaTopicName = kafkaService.credentials.get("topicName")
+
 print("  hostname",  kafkaServers)
 print("  topicName", kafkaTopicName)
 
@@ -17,4 +22,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def get_kafka_env():
-    return jsonify(kafka.credentials)
+    if kafkaService is None:
+        return jsonify({"hostname": kafkaServers, "topicName": kafkaTopicName})
+    else:
+        return jsonify(kafkaService.credentials)
