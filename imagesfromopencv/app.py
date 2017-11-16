fed3e9f6855ffd527a22721db557137576816ee4
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-16 14:50:02 +1000

    imagesfromopencv grabs frames from opencv local camera or 1/2/3 stock images

---
+++ imagesfromopencv/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
+
+camerastream = Camera()
+while True:
+    frame = camerastream.get_frame()
+    print("frame")
