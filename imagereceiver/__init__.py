08490625d8792c821c06dd19dda269db0c8d9fdb
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-16 14:28:34 +1000

    add statusconsumer to manifest.yml

--- imagereceiver/__init__.py
+++ imagereceiver/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, make_response
 from cfenv import AppEnv
 
 env = AppEnv()
-statusService = env.get_service(name='status')
+statusService = env.get_service(name='status-topic')
 if statusService is None:
     statusKafka = "localhost:9092"
     statusTopic  = "opencv-kafka-demo-status"
@@ -11,7 +11,7 @@ else:
     statusKafka  = statusService.credentials.get("hostname")
     statusTopic  = statusService.credentials.get("topicName")
 
-imagesService = env.get_service(name='images')
+imagesService = env.get_service(name='raw-images-topic')
 if imagesService is None:
     imagesKafka = "localhost:9092"
     imagesTopic = "opencv-kafka-demo-images"
--- statusconsumer/app.py
+++ statusconsumer/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, make_response
 from cfenv import AppEnv
 
 env = AppEnv()
-statusService = env.get_service(name='status')
+statusService = env.get_service(name='status-topic')
 if statusService is None:
     statusKafka = "localhost:9092"
     statusTopic  = "opencv-kafka-demo-status"
@@ -10,15 +10,6 @@ if statusService is None:
 else:
     statusKafka  = statusService.credentials.get("hostname")
     statusTopic  = statusService.credentials.get("topicName")
-
-imagesService = env.get_service(name='images')
-if imagesService is None:
-    imagesKafka = "localhost:9092"
-    imagesTopic = "opencv-kafka-demo-images"
-
-else:
-    imagesKafka  = imagesService.credentials.get("hostname")
-    imagesTopic  = imagesService.credentials.get("topicName")
 
 from kafka import KafkaConsumer
 statusConsumer = KafkaConsumer(statusTopic, bootstrap_servers=statusKafka)
