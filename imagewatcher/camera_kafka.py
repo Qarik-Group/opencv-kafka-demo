5b2af509ac1071ee7b72483e5b023aff22b49ae3
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-19 17:40:46 +1000

    ensure imagewatcher uses kafka for images

--- imagewatcher/camera_kafka.py
+++ imagewatcher/camera_kafka.py
@@ -1,8 +1,12 @@
 from camera_base import BaseCamera
 from cfenv import AppEnv
 
+cf_kafka_topic='objectdetector-images-topic'
+if os.environ.get('IMAGE_TOPIC_SERVICE_NAME'):
+    cf_kafka_topic = os.environ['IMAGE_TOPIC_SERVICE_NAME']
+
 env = AppEnv()
-imagesService = env.get_service(name='objectdetector-images-topic')
+imagesService = env.get_service(name=cf_kafka_topic)
 if imagesService is None:
     imagesKafka = "localhost:9092"
     imagesTopic = "opencv-kafka-demo-objectdetector-images"
