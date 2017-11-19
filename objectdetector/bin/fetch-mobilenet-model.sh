58d12eeec66fb39252b1ac2de03612582e9c195c
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-19 19:44:01 +1000

    show error if missing AWS env vars

--- objectdetector/bin/fetch-mobilenet-model.sh
+++ objectdetector/bin/fetch-mobilenet-model.sh
@@ -3,4 +3,7 @@
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 cd $ROOT
 
+: ${AWS_ACCESS_KEY_ID:?required}
+: ${AWS_SECRET_ACCESS_KEY:?required}
+
 aws s3 sync s3://opencv-buildpack/demos/cnn-example/ models/
