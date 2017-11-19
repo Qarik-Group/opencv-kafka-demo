d662217ebdd26412a94bfb25a5bc23a214eb4cec
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-20 09:57:27 +1000

    watch a specific topic via env var

--- imagewatcher/camera_kafka.py
+++ imagewatcher/camera_kafka.py
@@ -2,18 +2,22 @@ from camera_base import BaseCamera
 from cfenv import AppEnv
 import os
 
-cf_kafka_topic='objectdetector-images-topic'
+lookupServiceName='objectdetector-images-topic'
 if os.environ.get('IMAGE_TOPIC_SERVICE_NAME'):
-    cf_kafka_topic = os.environ['IMAGE_TOPIC_SERVICE_NAME']
+    lookupServiceName = os.environ['IMAGE_TOPIC_SERVICE_NAME']
 
 env = AppEnv()
-imagesService = env.get_service(name=cf_kafka_topic)
+imagesService = env.get_service(name=lookupServiceName)
 if imagesService is None:
     imagesKafka = "localhost:9092"
     imagesTopic = "opencv-kafka-demo-objectdetector-images"
+    if os.environ.get('IMAGE_TOPIC'):
+        imagesTopic = os.environ['IMAGE_TOPIC']
 else:
     imagesKafka  = imagesService.credentials.get("hostname")
     imagesTopic  = imagesService.credentials.get("topicName")
+
+print("Loading images from Kafka topic", imagesTopic)
 
 from kafka import KafkaConsumer
 imagesConsumer = KafkaConsumer(imagesTopic, bootstrap_servers=imagesKafka)
