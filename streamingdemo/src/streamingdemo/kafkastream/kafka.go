77288255234b2846386e5943e18f149db4fec5b0
Dr Nic Williams <drnicwilliams@gmail.com>
2017-11-23 09:03:40 +1000

    streamingdemo can discovery device IDs from bound service instances

--- streamingdemo/src/streamingdemo/kafkastream/kafka.go
+++ streamingdemo/src/streamingdemo/kafkastream/kafka.go
@@ -3,8 +3,11 @@ package kafkastream
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"streamingdemo/mjpeg"
 	"strings"
+
+	cfenv "github.com/cloudfoundry-community/go-cfenv"
 )
 
 // KafkaTopicStreams is the collection of KafkaTopics being watched for new frames
@@ -30,13 +33,63 @@ func NewKafkaTopicStreams() (streams *KafkaTopicStreams) {
 // DiscoverDeviceIDs looks at environment variables to discover which Kafka topics
 // the application should be watching, and which mjpeg.Stream to construct for viewing
 func (streams *KafkaTopicStreams) discoverDeviceIDs() {
+	if cfenv.IsRunningOnCF() {
+		cfApp, err := cfenv.Current()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR looking up CF services: %s", err)
+			os.Exit(1)
+		}
+
+		rawServices, err := cfApp.Services.WithNameUsingPattern("raw-images-topic-.*")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "At least one topic named 'raw-images-topic-<device-id>' must be bound to app.")
+			os.Exit(1)
+		}
+		streams.setupCFKafka(rawServices)
+		return
+	}
 	if os.Getenv("DEVICE_IDS") != "" {
 		streams.DeviceIDs = strings.Split(os.Getenv("DEVICE_IDS"), ",")
 		streams.setupLocalKafka()
 		return
 	}
-	fmt.Fprintf(os.Stderr, "No support for Cloud Foundry yet. Must set $DEVICE_IDS.")
+	fmt.Fprintf(os.Stderr, "Must set $DEVICE_IDS if not running within Cloud Foundry.")
 	os.Exit(1)
+}
+
+// setupCFKafka uses Cloud Foundry services to access Kafka topics for each image stream
+func (streams *KafkaTopicStreams) setupCFKafka(rawServices []cfenv.Service) {
+	regex := regexp.MustCompile("(?i)^raw-images-topic-(.*)$")
+
+	deviceIDs := make([]string, len(rawServices))
+	for i, rawService := range rawServices {
+		deviceID := regex.FindStringSubmatch(rawService.Name)[1]
+		deviceIDs[i] = deviceID
+	}
+	streams.DeviceIDs = deviceIDs
+
+	cfApp, _ := cfenv.Current()
+
+	for _, streamType := range streams.StreamTypes {
+		for _, deviceID := range streams.DeviceIDs {
+			serviceName := fmt.Sprintf("%s-images-topic-%s", streamType, deviceID)
+			service, err := cfApp.Services.WithName(serviceName)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to find service '%s': %v", serviceName, err)
+				os.Exit(1)
+			}
+			hostname, _ := service.CredentialString("hostname")
+			kafkaTopic, _ := service.CredentialString("topicName")
+
+			mjpegStreamKey := streams.mjpegStreamKey(streamType, deviceID)
+			mjpegStream := mjpeg.NewStream()
+			streams.MJPEGStreams[mjpegStreamKey] = mjpegStream
+
+			streams.ImageTopics[mjpegStreamKey] = NewImageTopic(hostname, kafkaTopic, mjpegStream)
+
+			fmt.Printf("Watching Cloud Foundry Kafka service %s for stream %s\n", serviceName, mjpegStreamKey)
+		}
+	}
 }
 
 // setupLocalKafka assumes the topic name for each streamType/deviceID with local kafka
